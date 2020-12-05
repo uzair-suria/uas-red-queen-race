63fa6cf5d3a977dda3b08e5be122ba27390a8e50
Uzair Suria <uzair.a.suria@outlook.com>
2020-12-05 18:37:36 +0500

    Create assetExports.js

---
+++ src/data/assetExports.js
@@ -0,0 +1,55 @@
+import runningAlice from '../assets/sprite_running-alice-queen_small.png';
+
+import bushSmall from '../assets/bush_small.png';
+import bush from '../assets/bush.png';
+
+import palm1Small from '../assets/palm1_small.png';
+import palm1 from '../assets/palm1.png';
+
+import palm2 from '../assets/palm2.png';
+import palm2Small from '../assets/palm2_small.png';
+
+import palm3 from '../assets/palm3.png';
+import palm3Small from '../assets/palm3_small.png';
+
+import redKnight from '../assets/r_knight.png';
+import redKnightSmall from '../assets/r_knight_small.png';
+
+import redPawn from '../assets/r_pawn.png';
+import redPawnSmall from '../assets/r_pawn_small.png';
+
+import redPawnUpright from '../assets/r_pawn_upright.png';
+import redPawnUprightSmall from '../assets/r_pawn_upright_small.png';
+
+import whiteRook from '../assets/w_rook.png';
+import whiteRookSmall from '../assets/w_rook_small.png';
+
+import whiteRookUpright from '../assets/w_rook_upright.png';
+import whiteRookUprightSmall from '../assets/w_rook_upright_small.png';
+
+import skyBG from '../assets/bg_sky_1.png';
+import earthBG from '../assets/bg_earth.jpg';
+
+export {
+	runningAlice,
+	bushSmall,
+	bush,
+	palm1Small,
+	palm1,
+	palm2,
+	palm2Small,
+	palm3,
+	palm3Small,
+	redKnight,
+	redKnightSmall,
+	redPawn,
+	redPawnSmall,
+	redPawnUpright,
+	redPawnUprightSmall,
+	whiteRook,
+	whiteRookSmall,
+	whiteRookUpright,
+	whiteRookUprightSmall,
+	earthBG,
+	skyBG,
+};
